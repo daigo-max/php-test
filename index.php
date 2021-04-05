1436c483d9ae9a0082fa24af88e802ed70f50e40
daigo-max <daidaitakano@gmail.com>
2021-04-05 18:09:00 +0900

    push

--- index.php
+++ index.php
@@ -4,11 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP test</title>
 </head>
 <body>
     <?php
-    printf("こんばんは!");
+        $x = 1;
+        $score = 1000;
+        $myName = "だいご";
+
+        //コメントテスト
+        print "こんばんは!<br>";
+        print "こんばんは!<br>";
+    
+
     ?>
+    <script>
+        document.write("おやすみ")
+    </script>
 </body>
 </html>
